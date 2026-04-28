3ded8487850a4851b801998a8dcef5ec9ad825f3
huzaifaumer007 <khichihuzaifa@gmail.com>
2026-04-28 21:39:47 +0500

    changes

--- src/content/instagram-reel-downloader-free/InstagramReelDownloaderBlogContent.js
+++ src/content/instagram-reel-downloader-free/InstagramReelDownloaderBlogContent.js
@@ -2,7 +2,7 @@
 
 import React from "react";
 import Link from "next/link";
-import Image from "next/image";
+import Image from "next/image"; 
 
 const blogImageStyle = { width: "100%", height: "auto", marginBottom: "1.25rem" };
 
